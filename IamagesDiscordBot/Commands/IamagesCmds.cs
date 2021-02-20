2e347b95345bcbcf1a4f653e85d604447f829a81
Mirza Hidayat <mirza898@gmail.com>
2021-02-20 10:06:12 +0300

    🚀Added random img command

--- IamagesDiscordBot/Commands/IamagesCmds.cs
+++ IamagesDiscordBot/Commands/IamagesCmds.cs
@@ -17,5 +17,36 @@ namespace IamagesDiscordBot.Commands
             var msg = await ctx.Channel.SendMessageAsync("Pong! : measuring...").ConfigureAwait(false);
             await msg.ModifyAsync($"Pong! : {ctx.Client.Ping} ms").ConfigureAwait(false);
         }
+
+        [Command("imgrandom"), Description("Retrieves an unprivated image for the user")]
+        [GroupName(Group.Iamages)]
+        [Cooldown(2, 5, CooldownBucketType.Channel)]
+        public async Task Random(CommandContext ctx)
+        {
+            IamagesAPIWrapper api = new IamagesAPIWrapper();
+            var imgInfo = api.getRandom(); // random image info
+            string imgUrl = api.getImage(imgInfo.ImageID); //the image
+
+            string fileType = BotServices.ConvertToString(imgInfo.FileMime);
+            string purity = BotServices.ConvertToString(imgInfo.FileNSFW);
+
+            string desc =
+                $"**ID** = {imgInfo.ImageID}\n" +
+                $"**Description** = {imgInfo.FileDesc ?? "No description provided."}\n" +
+                $"**ImageType** = {fileType}\n" +
+                $"**Purity** = {purity}\n" +
+                $"**Dimensions** = {imgInfo.FileWidth} x {imgInfo.FileHeight}\n" +
+                $"**Created at** = {imgInfo.created_at}";
+
+            var embed = new DiscordEmbedBuilder()
+                .WithAuthor("Iamages Discord Bot", null, SharedData.LogoURL)
+                .WithFooter("Retrieved from Iamages API")
+                .WithColor(SharedData.defaultColour)
+                .WithTimestamp(DateTime.Now)
+                .WithDescription(desc)
+                .WithImageUrl(imgUrl);
+
+            await ctx.Channel.SendMessageAsync(embed: embed).ConfigureAwait(false);
+        }
     }
 }
